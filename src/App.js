62394fdad4ac460d21fbbf3c9587e0954e6abb85
Nadeem Fayaz <iamabnadeem99@gmail.com>
2024-01-06 21:04:13 +0530

    fixed error

--- src/App.js
+++ src/App.js
@@ -6,13 +6,13 @@ const initialFriends = [
     id: 118836,
     name: "Tufail Dar",
     image: "https://i.pravatar.cc/48?u=118836",
-    balance: -25,
+    balance: -250,
   },
   {
     id: 933372,
     name: "Nadeem Fayaz",
     image: "https://i.pravatar.cc/48?u=933372",
-    balance: 250,
+    balance: 253,
   },
   {
     id: 499476,
