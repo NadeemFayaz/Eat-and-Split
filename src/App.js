5cea9ed5fd6ec17478dad4b7ab02d9edd3caeb73
Nadeem Fayaz <iamabnadeem99@gmail.com>
2024-01-15 23:48:51 +0530

    fixed bug

--- src/App.js
+++ src/App.js
@@ -4,21 +4,21 @@ import { useState } from "react";
 const initialFriends = [
   {
     id: 118836,
-    name: "Tufail Dar",
+    name: "Tufail",
     image: "https://i.pravatar.cc/48?u=118836",
-    balance: -250,
+    balance: -25,
   },
   {
     id: 933372,
     name: "Nadeem Fayaz",
     image: "https://i.pravatar.cc/48?u=933372",
-    balance: 253,
+    balance: 2503,
   },
   {
     id: 499476,
     name: "Suhaib Dar",
     image: "https://i.pravatar.cc/48?u=499476",
-    balance: 350,
+    balance: 3550,
   },
 ];
 
